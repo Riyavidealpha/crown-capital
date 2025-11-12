7df0d685f0fc1b39aa4ca9bb0692008ccf67e7b0
Riyavidealpha <162101406+Riyavidealpha@users.noreply.github.com>
2025-11-12 21:49:30 +0530

    Add footer with trading risk message
    Added a footer with a marquee message about trading risks.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,14 +9,21 @@ export const Home = () => {
     return (
         <>
             <Navbar />
-            <div className="min-w-screen min-h-screen overflow-x-hidden
-                sccrollbar-hide
-                   overflow-y-auto  ">
+            <div className="min-w-screen min-h-[calc(100vh-100px)] px-6 
+                bg-linear-to-r from-[#232652] to-[#B89043]
+                flex items-center justify-center overflow-y-auto ">
 
                 <Outlet />
 
             </div>
-
+            <Footer />
+            <div class="fixed bottom-0 left-0 w-full bg-[#B89043] text-white py-2 overflow-hidden z-50">
+                <div class="whitespace-nowrap animate-marquee">
+                    <p class="inline-block text-sm font-medium tracking-wide px-4">
+                        Trading CFDs involves substantial risk, invest only what you can afford to lose.
+                    </p>
+                </div>
+            </div>
         </>
     );
 };
